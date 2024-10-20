66325f91be74c35743b122bcf85c72dafa525025
林玮 (Jade Lin) <linw1995@icloud.com>
2024-10-20 11:35:15 +0800

    Customize titlebar of window

--- src/app.rs
+++ src/app.rs
@@ -51,6 +51,23 @@ pub fn App() -> impl IntoView {
     };
 
     view! {
+        <div data-tauri-drag-region class="titlebar">
+          <div class="titlebar-button" id="titlebar-minimize">
+            <img
+              src="https://api.iconify.design/mdi:window-minimize.svg"
+              alt="minimize"
+            />
+          </div>
+          <div class="titlebar-button" id="titlebar-maximize">
+            <img
+              src="https://api.iconify.design/mdi:window-maximize.svg"
+              alt="maximize"
+            />
+          </div>
+          <div class="titlebar-button" id="titlebar-close">
+            <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+          </div>
+        </div>
         <main class="container">
             <h1>"Welcome to Tauri + Leptos"</h1>
 
