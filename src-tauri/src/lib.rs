804db6ad1f8b16e2eaa10ebabb1f5b8f40807fad
林玮 (Jade Lin) <linw1995@icloud.com>
2024-10-20 13:32:50 +0800

    Toggle window visibility while hovering the tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,12 +2,19 @@ use smart_hosts_bridge::NetworkEvent;
 use tauri::{
     menu::{Menu, MenuItem},
     tray::TrayIconBuilder,
+    AppHandle,
 };
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn monitor() -> NetworkEvent {
+fn monitor(_app: AppHandle) -> NetworkEvent {
     use NetworkEvent::*;
+
+    //use tauri::Manager;
+    //use tauri_plugin_positioner::{Position, WindowExt};
+
+    //let mut win = app.get_webview_window("main").unwrap();
+    //let _ = win.as_ref().window().move_window(Position::TrayCenter);
 
     Unknown
 }
@@ -15,9 +22,12 @@ fn monitor() -> NetworkEvent {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![monitor])
         .setup(|app| {
+            app.hide().unwrap();
+
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
             let menu = Menu::with_items(app, &[&quit_i])?;
 
@@ -25,6 +35,15 @@ pub fn run() {
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
                 .menu_on_left_click(true)
+                .on_tray_icon_event(|tray_handle, event| {
+                    tauri_plugin_positioner::on_tray_event(tray_handle.app_handle(), &event);
+                    use tauri::Manager;
+                    use tauri_plugin_positioner::{Position, WindowExt};
+
+                    let win = tray_handle.app_handle().get_webview_window("main").unwrap();
+                    let _ = win.as_ref().window().move_window(Position::TrayCenter);
+                    win.show().unwrap();
+                })
                 .build(app)?;
             Ok(())
         })
